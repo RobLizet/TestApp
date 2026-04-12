40819bf23e5e7cb7da510629228a026724298c9e
RobLizet <zweetzakken@gmail.com>
2026-04-12 16:54:44 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,20 @@
-const CACHE = 'signals-v2';
-const FILES = [
-  './index.html',
-  './manifest.json'
-];
+const CACHE = ‘arche-v1’;
+const FILES = [’./index.html’, ‘./manifest.json’];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
-  self.skipWaiting();
+self.addEventListener(‘install’, e => {
+e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener(‘activate’, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+)
+);
+self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener(‘fetch’, e => {
+e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
